d12f90c2923cf079a3bd602c897c17d7ca57bbaa
adamsherwin <adamlsherwin@gmail.com>
2020-07-12 01:02:40 -0400

    Adding slight optimizations and comments

--- src/dsp/CooleyTurkeyFFT.hpp
+++ src/dsp/CooleyTurkeyFFT.hpp
@@ -1,4 +1,3 @@
-// https://www.drdobbs.com/cpp/a-simple-and-efficient-fft-implementatio/199500857
 #include <algorithm>
 #include <cmath>
 #include <complex>
@@ -16,9 +15,9 @@ bool fft(double * input, double * output, std::size_t size)
   {
     std::complex<double> * output_complex = (std::complex<double> *) malloc(size * sizeof(std::complex<double>));
     fft_attempt(input, output_complex, size, 1);
-    for (int i = 0; i < size; ++i)
+    for (std::size_t k_index = 0; k_index < size; ++k_index)
     {
-      output[i] = std::abs(output_complex[i]);
+      output[k_index] = std::abs(output_complex[k_index]);
     } // for
   } // if
   else
@@ -31,15 +30,19 @@ bool fft(double * input, double * output, std::size_t size)
 
 void dft_attempt(double * input, std::complex<double> * output, std::size_t size, std::size_t stride)
 {
-  // Outer loop calculates each value of the DFT
+  // Saves computational time for this calculation in the exponential
+  double exponential_constant = -2 * M_PI / size;
+
+  // Loop over each index in the DFT to calculate
   for (std::size_t k_counter = 0; k_counter < size; ++k_counter)
   {
-    // Inner loop - iterate over samples
-    std::complex<double> temp;
+    // Need to ensure value is zero to start
     output[k_counter*stride] = std::complex<double>(0,0);
+
+    // Loop over each sample in the input data
     for (std::size_t n_counter = 0; n_counter < size; ++n_counter)
     {
-      output[k_counter*stride] += input[n_counter * stride] * std::exp(std::complex<double>(0, -2 * M_PI * (double)k_counter * n_counter / size));
+      output[k_counter*stride] += input[n_counter * stride] * std::exp(std::complex<double>(0, exponential_constant * k_counter * n_counter));
     } // for
   } // for
 } // dft_attempt
@@ -53,20 +56,38 @@ void fft_attempt(double * input, std::complex<double> * output, std::size_t size
     return;
   } // if
 
-  fft_attempt(input, output, size / 2, stride * 2);
-  fft_attempt(input + stride, output+stride, size / 2, stride * 2);
+  // Values that are calculated twice
+  std::size_t stride_times_two = stride * 2;
+  std::size_t size_over_two = size / 2;
+  std::size_t stride_times_size = stride * size;
 
+  // Calculate the FFT in two halves
+  //    Using the even indices of current FFT
+  fft_attempt(input, output, size_over_two, stride_times_two);
+  //    Using the off indices of the current FFT
+  fft_attempt(input + stride, output+stride, size_over_two, stride_times_two);
+
+  // As of now, we need a temporary vector to store the output values
   std::complex<double> * temp = (std::complex<double> *) malloc(size * sizeof(std::complex<double>));
+
+  // Saves computational time for this calculation in the exponential
+  double exponential_constant = -2.0 * M_PI / size;
+
+  // Iterate across each index of the output FFT vector
   for (std::size_t k_index = 0; k_index < size; ++k_index)
   {
-    temp[k_index] = std::complex<double>(0,0);
-    temp[k_index] = output[ ((k_index%size) * stride*2) % (stride*size)] + std::exp(std::complex<double>(0, -2.0 * M_PI * k_index / size)) * output[(stride+((k_index)*stride)*2) % (stride*size)];
+    temp[k_index] = output[ (k_index * stride_times_two) % stride_times_size]
+                  + std::exp(std::complex<double>(0, exponential_constant * k_index))
+                  * output[(stride+(k_index*stride_times_two)) % stride_times_size];
   } // for
 
+  // As of now, we need to copy the temporary vector into the output vector.
+  // Cannot use memcpy because values usually are not contiguous due to stride.
   for (std::size_t k_index = 0; k_index < size; ++k_index)
   {
     output[k_index * stride] = temp[k_index];
   } // for
 
+  // Free allocated temporary vector
   free(temp);
 } // fft_attempt
