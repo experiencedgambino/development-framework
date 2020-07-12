0589c5ecd57f4fd9b1e0adcbd03dbc45d589ca2e
adamsherwin <adamlsherwin@gmail.com>
2020-07-12 00:27:15 -0400

    Working FFT. Next step - optimize

--- src/dsp/CooleyTurkeyFFT.hpp
+++ src/dsp/CooleyTurkeyFFT.hpp
@@ -3,25 +3,28 @@
 #include <cmath>
 #include <complex>
 #include <iostream>
-#include <thread>
 
 void dft_attempt(double * input, std::complex<double> * output, std::size_t size, std::size_t stride);
-bool fft_attempt(double * input, std::complex<double> * output, std::size_t size, std::size_t stride);
+void fft_attempt(double * input, std::complex<double> * output, std::size_t size, std::size_t stride);
 
 bool fft(double * input, double * output, std::size_t size)
 {
   bool return_value {true};
-  std::complex<double> * output_complex = (std::complex<double> *) malloc(size * sizeof(std::complex<double>));
+
+  // Compute if size divisible by 2
   if (size % 2 == 0)
   {
-    // Process if size divisible by 2
-    return_value &= fft_attempt(input, output_complex, size, 1);
+    std::complex<double> * output_complex = (std::complex<double> *) malloc(size * sizeof(std::complex<double>));
+    fft_attempt(input, output_complex, size, 1);
+    for (int i = 0; i < size; ++i)
+    {
+      output[i] = std::abs(output_complex[i]);
+    } // for
   } // if
-
-  for (int i = 0; i < size; ++i)
+  else
   {
-    output[i] = std::abs(output_complex[i]);
-  } // for
+    return_value = false;
+  } // else
 
   return return_value;
 } // fft
@@ -42,14 +45,12 @@ void dft_attempt(double * input, std::complex<double> * output, std::size_t size
 } // dft_attempt
 
 // Assumes output and data are same
-bool fft_attempt(double * input, std::complex<double> * output, std::size_t size, std::size_t stride)
+void fft_attempt(double * input, std::complex<double> * output, std::size_t size, std::size_t stride)
 {
-  bool return_value {true};
-
   if (size <= 2)
   {
     dft_attempt(input, output, size, stride);
-    return true;
+    return;
   } // if
 
   fft_attempt(input, output, size / 2, stride * 2);
@@ -59,7 +60,7 @@ bool fft_attempt(double * input, std::complex<double> * output, std::size_t size
   for (std::size_t k_index = 0; k_index < size; ++k_index)
   {
     temp[k_index] = std::complex<double>(0,0);
-    temp[k_index] = output[ ((k_index%size) * stride*2) % size] + std::exp(std::complex<double>(0, -2.0 * M_PI * k_index / size)) * output[(stride+((k_index)*stride)*2) % size];
+    temp[k_index] = output[ ((k_index%size) * stride*2) % (stride*size)] + std::exp(std::complex<double>(0, -2.0 * M_PI * k_index / size)) * output[(stride+((k_index)*stride)*2) % (stride*size)];
   } // for
 
   for (std::size_t k_index = 0; k_index < size; ++k_index)
@@ -68,28 +69,4 @@ bool fft_attempt(double * input, std::complex<double> * output, std::size_t size
   } // for
 
   free(temp);
-
-  return return_value;
 } // fft_attempt
-
-
-
-
-
-/*
-
-  Xn           [0]   [1]   [2]   [3]   [4]   [5]   [6]   [7]
-
-  Keven        [0]         [2]         [4]         [6]
-  Kodd               [1]         [3]         [5]         [7]
-
- K[0]          [0]   [1]
- K[1]                      [2]   [3]
- K[2]                                  [4]   [5]
- K[3]                                              [6]   [7]
- K[4]          [0]   [1]
- K[5]                      [2]   [3]
- K[6]                                  [4]   [5]
- K[7]                                              [6]   [7]
-
-*/
--- test/dsp/CooleyTurkeyTest.cpp
+++ test/dsp/CooleyTurkeyTest.cpp
@@ -2,19 +2,21 @@
 
 int main()
 {
-  int size = 8;
+  int size = 65536;
   double * input = (double *) malloc(size * sizeof(double));
   double * output = (double *) malloc(size * sizeof(double));
 
+  double interval = 2 * M_PI / size;
+
   for (int i = 0; i < size; ++i)
   {
-    input[i] = i;
+    input[i] = sin(10000 * i * interval);
   } // for
 
   fft(input, output, size);
 
   for (int i = 0; i < size; ++i)
   {
-    std::cout << output[i] << std::endl;
+    std::cout << sin(4 * i * interval) << "," << output[i] << std::endl;
   } // for
 } // main
